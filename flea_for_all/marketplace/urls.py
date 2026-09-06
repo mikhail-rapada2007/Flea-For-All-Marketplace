7d2ad1641648dbb951d80e91afa0c372a0cb849a
Jemimah <jcsalucop@student.hau.edu.ph>
2026-09-06 23:02:40 +0800

    Add dedicated category detail page and update thumbnail navigation

--- flea_for_all/marketplace/urls.py
+++ flea_for_all/marketplace/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("faq/<int:pk>/delete/", views.delete_faq, name="delete_faq"),
     path("stores/<int:pk>/review/add/", views.add_review, name="add_review"),
     path("product/<int:pk>/", views.product_detail, name="product_detail"),
+    path('category/<str:category_slug>/', views.category_detail, name='category_detail'),
 ]
